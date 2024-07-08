c67fab80865a5c7a6152fbb092ab71f0e4c7b92e
Aashish Panthee <aashishpanthee060@gmail.com>
2024-07-08 13:40:50 +0545

    chore(inti): initialize project with FundMe contract with fund and withdraw function

---
+++ FundMe.sol
@@ -0,0 +1,14 @@
+// Get Funds from user
+// Withdraw Funds
+// Set a minimum funding value in USD
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract FundMe{
+
+    function fund() public {}
+
+    function withdraw() public {}
+    
+}
