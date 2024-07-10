8331403503a6c7524dddc63e4afb891bf9e82d16
Aashish Panthee <aashishpanthee060@gmail.com>
2024-07-10 16:24:24 +0545

    feat: add fund() function to get fund from users minimum of 5 USD.

--- FundMe.sol
+++ FundMe.sol
@@ -4,11 +4,35 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe{
 
-    function fund() public {}
+    uint256 minimumUSD = 5e18;
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
-    function withdraw() public {}
-    
+        // Allow users to send money 
+    function fund() public payable {
+        // Have a minimum $ sent
+        require(getConversionRate(msg.value) >= minimumUSD, "didn't send enough ETH");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+
+    }
+
+        //gets price $(USD) in terms of ETH(wei)
+    function getPrice() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 answer,,,)=priceFeed.latestRoundData();
+        return uint256(answer * 1e10); 
+    }
+
+        //converts incoming ETH(wei) to price $(USD)
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethAmount * ethPrice) / 1e18;
+        return ethAmountInUsd;
+    }
+
 }
