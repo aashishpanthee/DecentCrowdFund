f5b76691ce90f394b52db8e784c355863adc504a
Aashish Panthee <aashishpanthee060@gmail.com>
2024-07-11 16:35:59 +0545

    feat: add withdraw function to withdraw the collected fund in contract

--- FundMe.sol
+++ FundMe.sol
@@ -9,18 +9,42 @@ import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe{
     using PriceConverter for uint256;
-    uint256 minimumUSD = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+    address immutable i_owner;
+    
+    constructor(){
+        i_owner = msg.sender;
+    }
 
     // Allow users to send money 
     function fund() public payable {
 
         // Have a minimum $5 sent
-        require(msg.value.getConversionRate() >= minimumUSD, "didn't send enough ETH");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "didn't send enough ETH");
 
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        addressToAmountFunded[msg.sender] +=  msg.value;
+    }
+
+    // withdraw money
+    function withdraw() public onlyOwner {
+        for(uint256 funderIndex = 0; funderIndex<funders.length;funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+
+        //call is one of the method to send Eth from one account or contract to another. 
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    // modifier created for only Owner of this contract
+    modifier onlyOwner(){
+        require(msg.sender == i_owner, "Sender is not the owner !!");
+        _;
     }
 
 }
